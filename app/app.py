ef9d8d6614aff4a948e98a25d26bd44013ee33b7
Stephen Tsoi-A-Sue <stsoiasue@gmail.com>
2018-03-08 20:21:25 -0800

    adust for heatmap testing

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 # import Flask
 from flask import Flask, render_template, redirect, jsonify
-from model import session, Contracts, Top
+from model import session, Contracts
 
 # initialize flask app
 app = Flask(__name__)
@@ -40,37 +40,37 @@ def awards():
 
     return jsonify(contracts_array)
 
-# return list of top awards names
-@app.route('/top_awards')
-def top_awards():
+# # return list of top awards names
+# @app.route('/top_awards')
+# def top_awards():
 
-    # empty array to hold award data
-    top_contracts_array = []
+#     # empty array to hold award data
+#     top_contracts_array = []
 
-    # place each award in a dict. and add to awards_array
-    for top_contract in Top:
+#     # place each award in a dict. and add to awards_array
+#     for top_contract in Top:
 
-        contract_dict = {}
+#         contract_dict = {}
 
-        contract_dict['Awarding_Agency'] = top_contract.Awarding_Agency
-        contract_dict['Subtier_Agency'] = top_contract.Subtier_Agency
-        contract_dict['Subtier_Code'] = top_contract.Subtier_Code
-        contract_dict['Category'] = top_contract.Category
-        contract_dict['POP_City'] = top_contract.POP_City
-        contract_dict['POP_State'] = top_contract.POP_State
-        contract_dict['POP_Zip'] = top_contract.POP_Zip
-        contract_dict['Recipient_Name'] = top_contract.Recipient_Name
-        contract_dict['Total_Obligation'] = top_contract.Total_Obligation
-        contract_dict['Latitude'] = top_contract.Latitude
-        contract_dict['Longitude'] = top_contract.Longitude
-        # contract_dict['Description'] = top_contract.Description
-        # contract_dict['Contract_ID'] = top_contract.Contract_ID
-        # contract_dict['Percentage_of_Dept'] = top_contract.Percentage_of_Dept
-        # contract_dict['Percentage_of_Total'] = top_contract.Percentage_of_Total
+#         contract_dict['Awarding_Agency'] = top_contract.Awarding_Agency
+#         contract_dict['Subtier_Agency'] = top_contract.Subtier_Agency
+#         contract_dict['Subtier_Code'] = top_contract.Subtier_Code
+#         contract_dict['Category'] = top_contract.Category
+#         contract_dict['POP_City'] = top_contract.POP_City
+#         contract_dict['POP_State'] = top_contract.POP_State
+#         contract_dict['POP_Zip'] = top_contract.POP_Zip
+#         contract_dict['Recipient_Name'] = top_contract.Recipient_Name
+#         contract_dict['Total_Obligation'] = top_contract.Total_Obligation
+#         contract_dict['Latitude'] = top_contract.Latitude
+#         contract_dict['Longitude'] = top_contract.Longitude
+#         # contract_dict['Description'] = top_contract.Description
+#         # contract_dict['Contract_ID'] = top_contract.Contract_ID
+#         # contract_dict['Percentage_of_Dept'] = top_contract.Percentage_of_Dept
+#         # contract_dict['Percentage_of_Total'] = top_contract.Percentage_of_Total
 
-        top_contracts_array.append(contract_dict)
+#         top_contracts_array.append(contract_dict)
 
-    return jsonify(top_contracts_array)
+#     return jsonify(top_contracts_array)
 
 @app.route('/top10')
 def top10():
